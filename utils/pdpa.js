7fd319c844c831ecfd58fa0ed355e214c837d8f1
Anawin W <imonmac@hotmail.com>
2020-05-12 01:52:30 +0700

    Add saveConsent function

--- utils/pdpa.js
+++ utils/pdpa.js
@@ -1,3 +1,7 @@
 export const toggleName = 'PDPA:accepted'
 
 export const isAccepted = () => localStorage.getItem(toggleName) === 'true'
+
+export const saveConsent = (accepted = true) => {
+  localStorage.setItem(toggleName, accepted)
+}
